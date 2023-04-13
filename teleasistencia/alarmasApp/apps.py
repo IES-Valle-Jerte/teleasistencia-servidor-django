8c843926a505a51bf6c9cd67b2c766e7fcdd7834
Aser Granado Amores <agranadoa09@informatica.iesvalledeljerteplasencia.es>
2023-04-13 16:55:27 +0200

    Mejora del scheduler de alarmas

--- teleasistencia/alarmasApp/apps.py
+++ teleasistencia/alarmasApp/apps.py
@@ -14,6 +14,11 @@ class AlarmasAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'alarmasApp'
 
+    def __init__(self, app_name, app_module):
+        super().__init__(app_name, app_module)
+        # Atributos adicionales
+        self.parada_scheduler = None # threading.Event que nos permitirá parar el hilo
+
     def ready(self):
         """
             Al iniciar la APP, gestionar los eventos de la BBDD.
@@ -26,19 +31,43 @@ class AlarmasAppConfig(AppConfig):
             procesar_alarmas_programadas()
             print("[\033[33m%s\033[0m]: Procesadas alarmas pendientes" % ('AlarmasApp'))
             # Iniciamos el sheduler para que gestione la tarea de las alarmas programadas
-            self.run_scheduler()
+            self.parada_scheduler = run_scheduler(49) # Realentizamos el scheduler
+            # Registramos el evento para que cuando django se esté cerrando, se pare el scheduler
+            # TODO
 
         # Este try-catch es para evitar que se procesen las alarmas cuando la app se inicia para gestionar migraciones
-        except Exception: pass
+        except Exception:
+            # Paramos el hilo
+            if self.parada_scheduler is not None:
+                self.parada_scheduler.set()
 
 
+def run_scheduler(intervalo=1):
+    """
+        Ejecuta en un hilo separado un bucle que mantiene ejecutando el scheduler,
+        cada `intervalo` comprobará si hay tareas pendientes (metodos marcados con @repeat).
 
-    def run_scheduler(self):
-        """
-            Ejecuta en un hilo separado un bucle que mantiene ejecutando el scheduler
-        """
-        hilo_tareas = threading.Thread(target=scheduler, daemon=True)
-        hilo_tareas.start()
+        NOTA: El intervalo deberá ser igual o inferior al tiempo de repetición de la tarea más rápida.
+        Si tenemos una tarea que se ejecuta cada segundo, este deberá ser de 1.
+
+        @param intervalo: tiempo en segundos
+        @return cease_continuous_run: threading.Event con el que podremos parar el hilo.
+    """
+    parar_scheduler = threading.Event()
+
+    # Clase del hilo que ejecutará el scheduler
+    class ScheduleThread(threading.Thread):
+        @classmethod
+        def run(cls):
+            while not parar_scheduler.is_set():
+                schedule.run_pending()
+                time.sleep(intervalo)
+
+    # Creamos el hilo
+    continuous_thread = ScheduleThread(daemon=True)
+    continuous_thread.start()
+    # Devolvemos el evento con el que podemos parar el scheduler
+    return parar_scheduler
 
 
 @repeat(every(1).minute)
@@ -54,9 +83,3 @@ def procesar_alarmas_programadas():
     pendientes = modelo_alarmas.objects.filter(query_pendientes)
     for alarma in pendientes:
         alarma.disparar()
-
-
-def scheduler():
-    while True:
-        schedule.run_pending()
-        time.sleep(10) # Realentizo el bucle manualmente para ahorrar recursos
